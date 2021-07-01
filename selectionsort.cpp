bd5849c58970a99bb74269d9b5fd761ae7f1942d
sakchi <sakshikumari1027@gmail.com>
2021-07-01 20:09:27 +0530

    Update selectionsort.cpp

--- selectionsort.cpp
+++ selectionsort.cpp
@@ -1,9 +1,9 @@
 //part of code for selection sort algorithm
-for(i=0;i<n-1;i++)
+for(int i=0;i<n-1;i++)
 {
     int min=i;
    
-    for(j=i+1;j<n,j++)
+    for(int j=i+1;j<n,j++)
      {
          if(a[j]<a[min])
           {
