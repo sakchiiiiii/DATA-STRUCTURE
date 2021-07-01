eaaced5a02ad0f6797227bcc66b1a6435bfc2330
sakchi <sakshikumari1027@gmail.com>
2021-07-01 20:08:08 +0530

    Create selectionsort.cpp

---
+++ selectionsort.cpp
@@ -0,0 +1,18 @@
+//part of code for selection sort algorithm
+for(i=0;i<n-1;i++)
+{
+    int min=i;
+   
+    for(j=i+1;j<n,j++)
+     {
+         if(a[j]<a[min])
+          {
+              min=j;
+          }
+        if(min!=i)
+          {
+              swap(a[i],a[min]); //built-in function foe swap function
+              
+          }
+     }
+}
